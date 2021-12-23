4ce3da8671fc5251c5493ea6e35005013ae87647
Theo Patron <patron.theo@gmail.com>
2021-12-23 13:01:23 +0100

    fix

--- preprocessing/stabilization.py
+++ preprocessing/stabilization.py
@@ -8,11 +8,11 @@ def find_rest_vids(path):
   res = []
   for up in os.listdir(path):
     if re.search(r'boulder_[1-7]_.*$', up):
-      for inside in os.listdir(up):
-        if len(inside) > 0 and os.path.isdir(f'{up}/{inside}'):
-          for v in os.listdir(f'{up}/{inside}'):
+      for inside in os.listdir(f'{path}{up}/'):
+        if len(inside) > 0 and os.path.isdir(f'{path}{up}/{inside}'):
+          for v in os.listdir(f'{path}{up}/{inside}'):
             if re.search(r'(^IMG_.*.MOV$)', v):
-              res.append(f'{up}/{inside}/{v}')
+              res.append(f'{path}{up}/{inside}/{v}')
 
   return res
 
@@ -20,11 +20,11 @@ def find_rest_mp4(path):
   res = []
   for up in os.listdir(path):
     if re.search(r'boulder_[1-7]_.*$', up):
-      for inside in os.listdir(up):
-        if len(inside) > 0 and os.path.isdir(f'{up}/{inside}'):
-          for v in os.listdir(f'{up}/{inside}'):
+      for inside in os.listdir(f'{path}{up}/'):
+        if len(inside) > 0 and os.path.isdir(f'{path}{up}/{inside}'):
+          for v in os.listdir(f'{path}{up}/{inside}'):
             if re.search(r'(^MOV_.*.mp4$)|((^.*.MP4$))', v):
-              res.append(f'{up}/{inside}/{v}')
+              res.append(f'{path}{up}/{inside}/{v}')
 
   return res  
 
@@ -32,11 +32,11 @@ def find_stab_vids(path):
   res = []
   for up in os.listdir(path):
     if re.search(r'boulder_[1-7]_.*$', up):
-      for inside in os.listdir(up):
-        if len(inside) > 0 and os.path.isdir(f'{up}/{inside}'):
-          for v in os.listdir(f'{up}/{inside}'):
+      for inside in os.listdir(f'{path}{up}/'):
+        if len(inside) > 0 and os.path.isdir(f'{path}{up}/{inside}'):
+          for v in os.listdir(f'{path}{up}/{inside}'):
             if re.search(r'(^.*_STAB.MOV$)', v):
-              res.append(f'{up}/{inside}/{v}')
+              res.append(f'{path}{up}/{inside}/{v}')
 
   return res
 
@@ -44,17 +44,19 @@ def find_all_vids(path):
   res = []
   for up in os.listdir(path):
     if re.search(r'boulder_[1-7]_.*$', up):
-      for inside in os.listdir(up):
-        if len(inside) > 0 and os.path.isdir(f'{up}/{inside}'):
-          for v in os.listdir(f'{up}/{inside}'):
+      for inside in os.listdir(f'{path}{up}/'):
+        if len(inside) > 0 and os.path.isdir(f'{path}{up}/{inside}'):
+          for v in os.listdir(f'{path}{up}/{inside}'):
             if re.search(r'(^.*.MOV$)|(^MOV_.*.mp4$)|(^.*.MP4$)', v):
-              res.append(f'{up}/{inside}/{v}')
+              res.append(f'{path}{up}/{inside}/{v}')
 
   return res
 
 def find_unstab_vids(path):
   all_vids = find_all_vids(path)
   stab_vids = find_stab_vids(path)
+  print(all_vids)
+  print(stab_vids)
   non_stab_vids = list(set(all_vids) - set(stab_vids))
   left_vids = [v[:-9] for v in list(set([f'{v}_STAB.MOV' for v in non_stab_vids]) - set(stab_vids))]
   if len(left_vids) == 0:
